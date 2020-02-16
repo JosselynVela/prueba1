b2897d5c4b4e226356b06e99eb063bb17813ea33
JosselynVela <deojosselyn@hotmail.com>
2020-02-16 01:20:25 -0500

    uno

---
+++ pets/src/Controller/UserController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+use App\Form\RegisterType;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user", name="user")
+     */
+    public function register(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class,$user);
+
+
+
+        return $this->render('user/register.html.twig', [
+            'form'=> $form->createView()
+
+        ]);
+    }
+}
